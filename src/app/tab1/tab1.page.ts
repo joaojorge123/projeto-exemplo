bb16fc64d5b92fcf9b32ed1fc388feb69f17dd80
joaojorge123 <44472555+joaojorge123@users.noreply.github.com>
2019-07-19 08:49:21 -0300

    Adicionei imagens
    Imagens de países

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,26 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  private viagens:any[] = [{
+    pais: 'Brasil',
+    local: 'Fernando de Noronha',
+    imagem: 'https://planetaamerica.com/wp-content/uploads/2013/02/Fotolia_5681513_Subscription_Monthly_XL.jpg',
+    descricao: ''
+  },
+  {
+  pais: 'EUA',
+  local: 'Nova Iork',
+  imagem: 'https://planetaamerica.com/wp-content/uploads/2013/02/Fotolia_5681513_Subscription_Monthly_XL.jpg',
+  descricao: ''
+},
+{
+  pais: 'EUA',
+  local: 'Grand Cannyon',
+  imagem: 'https://www.aprendizdeviajante.com/wp-content/uploads/2010/10/grandcanyon-696x451.jpg',
+  descricao: ''
+}
+];
+
   constructor() {}
 
 }
